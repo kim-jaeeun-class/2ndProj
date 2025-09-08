25f278de2c6ea651985d9ce5006ae10fe2378c1e
김재은 <wodmsdl0117@gmail.com>
2025-09-08 23:18:48 +0900

    250908_파일만 생성해둠. 코드 작성은 내일중에.

--- code/proj_mes/src/main/java/Dao/WorkOrderDAO.java
+++ code/proj_mes/src/main/java/Dao/WorkOrderDAO.java
@@ -1,5 +1,27 @@
 package Dao;
 
-public class WorkOrderDAO {
+import java.sql.Connection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class WorkOrderDAO {
+	// 여기에서 DB 연결 및 SQL 실행
+	// SQL 실행은 CONTROLLER가 바로 호출해서 실행하는 게 아니라 SERVICE를 사이에 두고 수행
+	
+	Connection getConn() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+			
+			conn = dataFactory.getConnection();			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
 }
--- code/proj_mes/src/main/java/Dto/WorkOrderDTO.java
+++ code/proj_mes/src/main/java/Dto/WorkOrderDTO.java
@@ -1,5 +1,6 @@
 package Dto;
 
 public class WorkOrderDTO {
-
+	// 여기에서 변수 지정 하고 GETTER, SETTER, TOSTRING 설정
+	// 다른 식으로 얘기하자면 DB 테이블의 한 행을 담는 느낌?
 }
