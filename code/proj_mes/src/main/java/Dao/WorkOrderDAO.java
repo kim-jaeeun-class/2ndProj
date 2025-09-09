22746860b8a8649abb408e715227a3bbc35a23e0
DESKTOP-HDGPUFR\admin <wodmsdl0117@gmail.com>
2025-09-09 11:32:32 +0900

    250909_DTO 넣어두기 아마 완료, DAO 전체 조회만 넣어둠(김재은)

--- code/proj_mes/src/main/java/Dao/WorkOrderDAO.java
+++ code/proj_mes/src/main/java/Dao/WorkOrderDAO.java
@@ -1,10 +1,16 @@
 package Dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import Dto.WorkOrderDTO;
 
 public class WorkOrderDAO {
 	// 여기에서 DB 연결 및 SQL 실행
@@ -23,5 +29,100 @@ public class WorkOrderDAO {
 		}
 
 		return conn;
+	}
+	
+	// 전체 조회
+	public List<WorkOrderDTO> selectAllWO() {
+		List<WorkOrderDTO> listAll = new ArrayList<WorkOrderDTO>();
+		
+		try {
+			Connection conn = getConn();
+			
+			String query = "select"
+					+ "     wo_num, wo_date, wo_duedate, wo_pq, wo_aq, "
+					+ "     wo_ps, worker_id, item_code"
+					+ "     from work_order";
+			PreparedStatement ps = conn.prepareStatement(query);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				WorkOrderDTO dto = new WorkOrderDTO();
+				
+				dto.setWoNum(rs.getInt("wo_num"));
+				dto.setWoDate(rs.getDate("wo_date"));
+				dto.setWoDuedate(rs.getDate("wo_duedate"));
+				dto.setWoPQ(rs.getInt("wo_pq"));
+				dto.setWoAQ(rs.getInt("wo_aq"));
+				dto.setWoPS(rs.getString("wo_ps"));
+				dto.setWorkerID(rs.getString("worker_id"));
+				dto.setItemCode(rs.getString("item_code"));
+				
+				listAll.add(dto);
+			}
+			
+			rs.close();
+			ps.close();
+			conn.close();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return listAll;
+		
+	}
+	
+	// 필터링 조회 : 미진행/진행/완료(결제중, 미확인은 어디서 확인해야 하나...)
+	public List<WorkOrderDTO> selectOrderWO() {
+		List<WorkOrderDTO> listFilter = new ArrayList<WorkOrderDTO>();
+		
+		try {
+			Connection conn = getConn();
+			
+			String query = "select"
+					+ "     wo_num, wo_date, wo_duedate, wo_pq, wo_aq, "
+					+ "     wo_ps, worker_id, item_code"
+					+ "     from work_order"
+					+ "		where wo_ps = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			
+			// 여기부터가 좀... 아닌 것 같은데.
+			// 기존 전체 조회에서는 if문 안에 new WorkOrderDTO()를 넣었는데,
+			// 이렇게 빼도 되나? -> 안 된다 진짜...
+			// TODO : setString이 고민...
+			
+			ps.setString(1, WorkOrderDTO.getWoPS());
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				WorkOrderDTO dto = new WorkOrderDTO();
+				
+				dto.setWoNum(rs.getInt("wo_num"));
+				dto.setWoDate(rs.getDate("wo_date"));
+				dto.setWoDuedate(rs.getDate("wo_duedate"));
+				dto.setWoPQ(rs.getInt("wo_pq"));
+				dto.setWoAQ(rs.getInt("wo_aq"));
+				dto.setWoPS(rs.getString("wo_ps"));
+				dto.setWorkerID(rs.getString("worker_id"));
+				dto.setItemCode(rs.getString("item_code"));
+				
+				listFilter.add(dto);
+			}
+			
+			rs.close();
+			ps.close();
+			conn.close();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return listFilter;
+		
 	}
 }
--- code/proj_mes/src/main/java/Dto/ProPerfDTO.java
+++ code/proj_mes/src/main/java/Dto/ProPerfDTO.java
@@ -1,5 +1,75 @@
 package Dto;
 
-public class ProPerfDTO {
+import java.sql.Date;
 
+public class ProPerfDTO {
+	private String csID;		// 생산실적ID
+	private int woNum;			// 작업지시번호
+	private Date woDate;		// 작업지시서 작성일
+	private int csStat;			// 지시 상태
+	private Date csDate;		// 일자(작성일자)
+	private String csInType;	// 입고 형태
+	private String csOutType;	// 출고 형태
+	private String stockID;		// 재고 ID(LOT 대용) : FK
+	
+	
+	public String getCsID() {
+		return csID;
+	}
+	public void setCsID(String csID) {
+		this.csID = csID;
+	}
+	public int getWoNum() {
+		return woNum;
+	}
+	public void setWoNum(int woNum) {
+		this.woNum = woNum;
+	}
+	public int getCsStat() {
+		return csStat;
+	}
+	public void setCsStat(int csStat) {
+		this.csStat = csStat;
+	}
+	public Date getCsDate() {
+		return csDate;
+	}
+	public void setCsDate(Date csDate) {
+		this.csDate = csDate;
+	}
+	public String getCsInType() {
+		return csInType;
+	}
+	public void setCsInType(String csInType) {
+		this.csInType = csInType;
+	}
+	public String getCsOutType() {
+		return csOutType;
+	}
+	public void setCsOutType(String csOutType) {
+		this.csOutType = csOutType;
+	}
+	public String getStockID() {
+		return stockID;
+	}
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	public Date getWoDate() {
+		return woDate;
+	}
+	public void setWoDate(Date woDate) {
+		this.woDate = woDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "ProPerfDTO [csID=" + csID + ", woNum=" + woNum + ", woDate=" + woDate + ", csStat=" + csStat
+				+ ", csDate=" + csDate + ", csInType=" + csInType + ", csOutType=" + csOutType + ", stockID=" + stockID
+				+ "]";
+	}
+
+	
+	
+	
 }
