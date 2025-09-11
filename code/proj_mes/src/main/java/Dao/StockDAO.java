7cd16e60fddb190fcae860cde09c392ed53ebce8
SeongYeonYun <69850705+SeongYeonYun@users.noreply.github.com>
2025-09-11 23:56:49 +0900

    재고 DAO, DTO 메소드 및 변수 추가

--- code/proj_mes/src/main/java/Dao/StockDAO.java
+++ code/proj_mes/src/main/java/Dao/StockDAO.java
@@ -34,6 +34,126 @@ public class StockDAO {
 		
 		return conn;
 	}
+	
+	
+
+//	select s.STOCK_ID, s.STOCK_DATE, s.STOCK_LOC, s.STOCK_DIV, s.STOCK_STAT, s.STOCK_NUMBER, i.ITEM_CODE, i.item_price
+//	from stock s
+//	join item i on s.item_code = i.item_code;
+
+	
+	
+	//////////////////////////////////  stock_list .html////////////////////////////////////////////////////////////
+	
+	//분류의 기준은 분명 품목 코드일 것이다.
+	//그럼 이걸 품목에서 추가해 주어야 하는가??
+	//그럼 이걸 품목에서 비고로 넣는건 어떨까??
+	//차라리 품목 코드를 가져온후 글자2의 크기로 잘라 맵핑하자 2/2/2+@
+	
+//	아니면 그냥 조건이 주어지면 대분류 + 중분류 + 소분류 = 품목코드니까 문자열 위치 찾기 같은 기능으로 분류가 가능했을듯....?
+			
+	
+	
+//	select s.STOCK_ID, s.STOCK_DATE, s.STOCK_LOC, s.STOCK_DIV, s.STOCK_STAT, s.STOCK_NUMBER, i.ITEM_CODE, i.item_price
+//	from stock s
+//	join item i on s.item_code = i.item_code;
+	
+	
+	// select
+	public List stock_list_menu() {
+		
+		List list = new ArrayList();
+		
+		try {
+			
+			// DB 접속
+			Connection con = getConn();
+			
+			// SQL 준비
+			//itemcode를 기반으로 item table 조인
+			//차라리 품목 코드를 가져온후 글자2의 크기로 잘라 맵핑하자 2/2/2+@
+			//목록에서 조회를 할떄 
+			String query = 
+					" SELECT "
+					+ "  s.STOCK_ID, s.STOCK_DATE, s.STOCK_LOC, s.STOCK_DIV, s.STOCK_STAT, s.STOCK_NUMBER, "
+					+ "  i.ITEM_CODE, i.ITEM_PRICE, "
+					+ "  CASE WHEN LENGTH(i.ITEM_CODE)    >= 2 THEN SUBSTR(i.ITEM_CODE, 1, 2) END AS big_category, "
+					+ "  CASE WHEN LENGTH(i.ITEM_CODE)    >= 4 THEN SUBSTR(i.ITEM_CODE, 3, 2) END AS mid_category, "
+					+ "  CASE WHEN LENGTH(i.ITEM_CODE)    >= 5 THEN SUBSTR(i.ITEM_CODE, 5)     END AS small_category "
+					+ "FROM stock s "
+					+ "JOIN item  i ON s.item_code = i.item_code";
+			PreparedStatement ps = con.prepareStatement(query);
+			
+	
+			
+			
+			// SQL 실행
+			ResultSet rs = ps.executeQuery();
+
+			// 결과 활용
+			while (rs.next()) {
+			    StockDTO dto = new StockDTO();
+
+			    dto.setStock_id(rs.getString("stock_id"));
+			    dto.setStock_date(rs.getDate("stock_date"));
+			    dto.setStock_loc(rs.getInt("stock_loc"));
+			    dto.setStock_div(rs.getInt("stock_div"));
+			    dto.setStock_number(rs.getInt("stock_number"));
+			    dto.setItem_code(rs.getString("item_code"));
+
+		
+			 		    
+			    
+			    
+			    
+			    dto.setItem_price(rs.getString("item_price"));
+
+			    // 새로 추가된 파생 컬럼들
+			    dto.setBigCategory(rs.getString("big_category"));
+			    dto.setMidCategory(rs.getString("mid_category"));
+			    dto.setSmallCategory(rs.getString("small_category"));
+
+			    list.add(dto);
+			}
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 	// select
--- code/proj_mes/src/main/java/Dto/StockDTO.java
+++ code/proj_mes/src/main/java/Dto/StockDTO.java
@@ -9,7 +9,47 @@ public class StockDTO {
 	private int stock_loc;		// 위치
 	private int stock_div;		// 구분
 	private int stock_number;	// 수량
-	private String item_code;	// 품목코드	
+	private String item_code;	// 품목코드
+	
+	
+	//새로 추가됨, item_code를 2/2/2+@로 더해서 나눈 분류
+    private String bigCategory; //대분류
+    private String midCategory; //중분류
+    private String smallCategory; //소분류
+    
+    
+    
+    //item 테이블의 단가
+    private String item_price;
+	
+    
+    
+    public String getBigCategory() {
+		return bigCategory;
+	}
+	public void setBigCategory(String bigCategory) {
+		this.bigCategory = bigCategory;
+	}
+	public String getMidCategory() {
+		return midCategory;
+	}
+	public void setMidCategory(String midCategory) {
+		this.midCategory = midCategory;
+	}
+	public String getSmallCategory() {
+		return smallCategory;
+	}
+	public void setSmallCategory(String smallCategory) {
+		this.smallCategory = smallCategory;
+	}
+	public String getItem_price() {
+		return item_price;
+	}
+	public void setItem_price(String item_price) {
+		this.item_price = item_price;
+	}
+
+	
 	
 	
 	public String getStock_id() {
@@ -52,7 +92,11 @@ public class StockDTO {
 	@Override
 	public String toString() {
 		return "StockDTO [stock_id=" + stock_id + ", stock_date=" + stock_date + ", stock_loc=" + stock_loc
-				+ ", stock_div=" + stock_div + ", stock_number=" + stock_number + ", item_code=" + item_code + "]";
+				+ ", stock_div=" + stock_div + ", stock_number=" + stock_number + ", item_code=" + item_code
+				+ ", bigCategory=" + bigCategory + ", midCategory=" + midCategory + ", smallCategory=" + smallCategory
+				+ ", item_price=" + item_price + "]";
 	}
+	
+
 	
 }
